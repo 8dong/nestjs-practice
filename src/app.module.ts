80b18e2bf9a6643b3fbbf444ce33baa328975c74
8dong <hyde981114@gmail.com>
2023-03-05 03:10:04 +0900

    feat: MySQL 연결 및 환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT as unknown as number,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      synchronize: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
