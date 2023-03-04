b72a6e68abc61c58f9f9eca16e4f5780c9aedae6
8dong <hyde981114@gmail.com>
2023-03-04 18:39:11 +0900

    feat: cats 모듈 생성 및 등록

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Put, Patch, Delete } from '@nestjs/common';
+
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCats() {
+    return 'all cats';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update partial cat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete cat';
+  }
+}
