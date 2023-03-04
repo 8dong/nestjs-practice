fea1773cfb7fd2c3dc37a03e9a0d26f4fcc2e01d
8dong <hyde981114@gmail.com>
2023-03-05 04:44:25 +0900

    feat: signUp service 로직 추가

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { CatEntity } from './cats.entity';
+import { CatRequestDto } from './dto/cats.request.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  constructor(
+    @InjectRepository(CatEntity)
+    private readonly catEntity: Repository<CatEntity>,
+  ) {}
+
+  async signUp(body: CatRequestDto) {
+    const { email, password, name } = body;
+    const isCatExist = await this.catEntity.exist({ where: { email } });
+
+    if (isCatExist) {
+      throw new UnauthorizedException('Cat already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const cat = this.catEntity.save({
+      email,
+      password: hashedPassword,
+      name,
+    });
+
+    return cat;
+  }
+}
